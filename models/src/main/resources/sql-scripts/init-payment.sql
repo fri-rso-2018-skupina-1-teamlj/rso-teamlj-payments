6b0fbcf76759c6eed84f3206f3b279fa84b5b928
ladislavskufca <ladislavskufca@users.noreply.github.com>
2019-01-04 22:54:36 +0100

    init sql change

--- models/src/main/resources/sql-scripts/init-payment.sql
+++ models/src/main/resources/sql-scripts/init-payment.sql
@@ -1,3 +1,3 @@
 INSERT INTO payments (dateOfPayment, endOfSubscription, subscription, userID) VALUES ('2018-10-01 15:36:38', '2013-11-01 15:36:38', false, 3)
-INSERT INTO payments (dateOfPayment, endOfSubscription, subscription, userID) VALUES ('2013-10-02 15:36:38', '2013-12-02 15:36:38', true, 2)
+INSERT INTO payments (dateOfPayment, endOfSubscription, subscription, userID) VALUES ('2013-10-02 15:36:38', '2013-12-02 15:36:38', false, 2)
 INSERT INTO payments (dateOfPayment, endOfSubscription, subscription, userID) VALUES ('2013-10-03 15:36:38', '2018-12-25 15:36:38', true, 1)
